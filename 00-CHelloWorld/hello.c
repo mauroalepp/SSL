310ea76966caf0db14248dfa86b29058b0367cfd
mpenapalacios <mpenapalacios@frba.utn.edu.ar>
2023-04-20 18:07:22 -0300

    Terminado el TP0

---
+++ 00-CHelloWorld/hello.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int imprimir (char *hello) {
+    printf("%s\n", hello);
+    return 0;
+}
+
+
+int escribirArchivo (char *hello, char *nombreArchivo, FILE *af){
+    af = fopen(nombreArchivo, "w");
+
+    if(af == NULL)
+    {
+        printf("Error abriendo el archivo");   
+        exit(1);             
+    }
+
+    fprintf(af,"%s", hello);
+    fclose(af);
+    return 0;
+}
+
+int verificarArchivo (char *hello, char *nombreArchivo, FILE *af){
+    char verificacion[30] = "";
+    char buffer=(long)"";
+    af = fopen(nombreArchivo, "r");
+    
+    if(af == NULL)
+    {
+        printf("Error abriendo el archivo");   
+        exit(1);             
+    }
+   
+   while ((buffer=getc(af))!=EOF)  
+    strncat(verificacion, &buffer, 1);
+
+    fclose(af);
+
+    if (!(strcmp(hello,verificacion)))
+        printf("El contenido de output.txt es el mismo del string: %s \n", hello);
+    else 
+        printf("El contenido de output.txt es diferente del string: %s \n", hello);
+    
+
+}
+
+
+int main(int argc, char* argv[]){
+    FILE *af = NULL;
+    char hello[30] = "Hello, World!";
+    char nombreArchivo[30] = "output.txt";
+    imprimir (hello);
+    escribirArchivo(hello, nombreArchivo, af);
+    verificarArchivo(hello, nombreArchivo, af);
+}
